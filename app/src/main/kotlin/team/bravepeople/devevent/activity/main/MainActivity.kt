c1501458f3b512e7e85be93b3d5df9943e939f07
Ji Sungbin <ji@sungb.in>
2021-06-25 01:24:57 +0900

    Remove duplicate copyright

--- app/src/main/kotlin/team/bravepeople/devevent/activity/main/MainActivity.kt
+++ app/src/main/kotlin/team/bravepeople/devevent/activity/main/MainActivity.kt
@@ -2,15 +2,6 @@
  * DevEventAndroid © 2021 용감한 친구들. all rights reserved.
  * DevEventAndroid license is under the MIT.
  *
- * [MainActivity.kt] created by Ji Sungbin on 21. 6. 24. 오전 12:11.
- *
- * Please see: https://github.com/brave-people/Dev-Event-Android/blob/master/LICENSE.
- */
-
-/*
- * DevEventAndroid © 2021 용감한 친구들. all rights reserved.
- * DevEventAndroid license is under the MIT.
- *
  * [MainActivity.kt] created by Ji Sungbin on 21. 6. 22. 오후 3:41.
  *
  * Please see: https://github.com/brave-people/Dev-Event-Android/blob/master/LICENSE.
