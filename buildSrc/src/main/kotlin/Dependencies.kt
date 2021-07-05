7a0c1e9385c341084504876d50303e89917b085f
jisungbin <ji@sungb.in>
2021-07-06 02:01:06 +0900

    Application version up

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -14,8 +14,8 @@ object Application {
     const val targetSdk = 30
     const val compileSdk = 30
     const val jvmTarget = "1.8"
-    const val versionCode = 5
-    const val versionName = "1.0.0"
+    const val versionCode = 6
+    const val versionName = "1.0.1"
 
     val targetCompat = JavaVersion.VERSION_11
     val sourceCompat = JavaVersion.VERSION_11
