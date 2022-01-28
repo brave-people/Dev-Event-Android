beef7781b0cd603f21bd4ce783f4619bbce45f0d
jisungbin <ji@sungb.in>
2022-01-28 18:02:55 +0900

    Bump gradle version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -28,7 +28,7 @@ object Versions {
         const val Kotlin = "1.6.0"
         const val CoreKtx = "1.7.0"
         const val Coroutines = "1.6.0"
-        const val Gradle = "7.1.0-rc01"
+        const val Gradle = "7.1.0"
     }
 
     object Network {
