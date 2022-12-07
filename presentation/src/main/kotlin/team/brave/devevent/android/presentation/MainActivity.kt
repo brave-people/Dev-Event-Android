025ab9fb21f1d3f0a067825ab7f9753784fb56ed
jisungbin <ji@sungb.in>
2022-12-08 00:28:05 +0900

    refactor: 코드 포멧팅

--- presentation/src/main/kotlin/team/brave/devevent/android/presentation/MainActivity.kt
+++ presentation/src/main/kotlin/team/brave/devevent/android/presentation/MainActivity.kt
@@ -16,10 +16,9 @@ import androidx.databinding.DataBindingUtil
 import team.brave.devevent.android.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
-    private lateinit var binding : ActivityMainBinding
-
-    override fun onCreate(savedInstanceState : Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
