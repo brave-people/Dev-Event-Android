cd883cdea69b3aed678b4647419bc95a116fd454
jisungbin <ji@sungb.in>
2023-01-01 15:41:23 +0900

    feat: data -> domain mapper 추가

---
+++ data/src/main/kotlin/team/brave/devevent/android/data/util/color.kt
@@ -0,0 +1,18 @@
+/*
+ * DevEvent-Android © 2022 Ji Sungbin. all rights reserved.
+ * DevEvent-Android license is under the MIT.
+ *
+ * [color.kt] created by Ji Sungbin on 23. 1. 1. 오후 3:28
+ *
+ * Please see: https://github.com/brave-people/Dev-Event-Android/blob/master/LICENSE
+ */
+
+package team.brave.devevent.android.data.util
+
+import java.util.Random
+
+// https://stackoverflow.com/a/35459935/14299073
+internal fun generateRandomHexColor(): String {
+    val nextInt = Random().nextInt(0xffffff + 1)
+    return String.format("#%06x", nextInt)
+}
