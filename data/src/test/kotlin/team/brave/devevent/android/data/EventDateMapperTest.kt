71282fd891c80f2cec8465e3cc8c51c0757495ff
jisungbin <ji@sungb.in>
2023-01-02 19:18:42 +0900

    test: EventDate 타입 추가 반영

--- data/src/test/kotlin/team/brave/devevent/android/data/EventDateMapperTest.kt
+++ data/src/test/kotlin/team/brave/devevent/android/data/EventDateMapperTest.kt
@@ -14,6 +14,7 @@ package team.brave.devevent.android.data
 import org.junit.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import team.brave.devevent.android.data.mapper.EventDate
 import team.brave.devevent.android.data.mapper.toEventDateString
 
 class EventDateMapperTest {
@@ -21,7 +22,7 @@ class EventDateMapperTest {
     fun `이벤트 카드 형식에 맞는 날짜 변환 - 다른 일정`() {
         val startDate = "2023-01-02T06:00"
         val endDate = "2023-01-03T08:00"
-        val dateString = (startDate to endDate).toEventDateString()
+        val dateString = (EventDate.from(startDate) to EventDate.from(endDate)).toEventDateString()
 
         val expected = "2023.01.02(월) ~ 2023.01.03(화)"
 
@@ -32,7 +33,7 @@ class EventDateMapperTest {
     fun `이벤트 카드 형식에 맞는 날짜 변환 - 같은 일정`() {
         val startDate = "2023-01-02T06:00"
         val endDate = "2023-01-02T08:00"
-        val dateString = (startDate to endDate).toEventDateString()
+        val dateString = (EventDate.from(startDate) to EventDate.from(endDate)).toEventDateString()
 
         val expected = "2023.01.02(월) 06:00 ~ 08:00"
 
