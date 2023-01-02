5ecbc443392d4afc4ed437b059b1e0d77253ea6c
jisungbin <ji@sungb.in>
2023-01-02 17:34:08 +0900

    chore: 테스트 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,4 +1,14 @@
+/*
+ * DevEvent-Android © 2022 Ji Sungbin. all rights reserved.
+ * DevEvent-Android license is under the MIT.
+ *
+ * [build.gradle.kts] created by Ji Sungbin on 23. 1. 2. 오후 5:32
+ *
+ * Please see: https://github.com/brave-people/Dev-Event-Android/blob/master/LICENSE
+ */
+
 import DependencyHandler.Extensions.implementations
+import DependencyHandler.Extensions.testImplementations
 
 plugins {
     id(ConventionEnum.AndroidLibrary)
@@ -15,5 +25,9 @@ dependencies {
         libs.kotlin.coroutines,
         libs.bundles.ktor,
         projects.domain,
+    )
+    testImplementations(
+        libs.test.coroutines,
+        libs.test.ktor.client,
     )
 }
