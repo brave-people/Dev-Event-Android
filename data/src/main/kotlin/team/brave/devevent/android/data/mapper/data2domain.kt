e8c8e6d8798a242bcd61c8ba9bda8dec1976ad84
jisungbin <ji@sungb.in>
2023-01-02 13:16:47 +0900

    feat: 이벤트 일정 mapper 작성

--- data/src/main/kotlin/team/brave/devevent/android/data/mapper/data2domain.kt
+++ data/src/main/kotlin/team/brave/devevent/android/data/mapper/data2domain.kt
@@ -9,6 +9,11 @@
 
 package team.brave.devevent.android.data.mapper
 
+import androidx.annotation.VisibleForTesting
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 import team.brave.devevent.android.data.model.EventResponseItem
 import team.brave.devevent.android.data.model.TagData
 import team.brave.devevent.android.data.util.generateRandomHexColor
@@ -20,14 +25,21 @@ internal fun List<EventResponseItem>.toDomain(): List<Event> {
     val events = mapNotNull { item ->
         val events = item.events?.filterNotNull() ?: return@mapNotNull null
         events.mapNotNull nestedMapNotNull@{ event ->
+            // require fields assertion
+            event.title ?: return@nestedMapNotNull null
+            event.organizer ?: return@nestedMapNotNull null
+            event.startDateTime ?: return@nestedMapNotNull null
+            event.endDateTime ?: return@nestedMapNotNull null
+            event.eventTimeType ?: return@nestedMapNotNull null
+            event.eventLink ?: return@nestedMapNotNull null
+
             Event(
-                title = event.title ?: return@nestedMapNotNull null,
-                organizer = event.organizer ?: return@nestedMapNotNull null,
-                time = TODO(),
-                timeType = event.eventTimeType?.let { type -> EventTimeType.valueOf(type) }
-                    ?: return@nestedMapNotNull null,
-                tags = event.tags?.filterNotNull()?.mapNotNull(TagData::toDomain) ?: emptyList(),
-                eventLink = event.eventLink ?: return@nestedMapNotNull null,
+                title = event.title,
+                organizer = event.organizer,
+                time = (event.startDateTime to event.endDateTime).toEventDateString() ?: "조회 실패",
+                timeType = EventTimeType.valueOf(event.eventTimeType),
+                tags = event.tags?.filterNotNull()?.mapNotNull(TagData::toDomain).orEmpty(),
+                eventLink = event.eventLink,
                 bannerUrl = event.coverImageLink.orEmpty(),
             )
         }
@@ -40,4 +52,49 @@ private fun TagData.toDomain(): Tag? {
         name = name ?: return null,
         hexColor = hexColor ?: generateRandomHexColor(),
     )
+}
+
+/**
+ * 이벤트 시작일과 종료일을 [Pair] 로 받고,
+ * 이벤트 카드에 표시할 형식에 맞게 다듬어 반환합니다.
+ *
+ * ### 표시 조건
+ *
+ * - 시작일과 종료일이 같은 경우: yyyy.MM.dd(E) HH:mm ~ HH:mm
+ * - 시작일과 종료일이 다른 경우: yyyy.MM.dd(E) ~ yyyy.MM.dd(E)
+ */
+@VisibleForTesting
+internal fun Pair<String, String>.toEventDateString(): String? {
+    val startDate = first.toDate() ?: return null
+    val endDate = second.toDate() ?: return null
+
+    return if (startDate.isSameDay(endDate)) {
+        // 시작일과 종료일이 같은 경우
+        val startTime = SimpleDateFormat("yyyy.MM.dd(E) HH:mm", Locale.KOREA).format(startDate)
+        val endTime = SimpleDateFormat("HH:mm", Locale.KOREA).format(endDate)
+        "$startTime ~ $endTime"
+    } else {
+        // 시작일과 종료일이 다른 경우
+        val dateFormat = SimpleDateFormat("yyyy.MM.dd(E)", Locale.KOREA)
+        val startTime = dateFormat.format(startDate)
+        val endTime = dateFormat.format(endDate)
+        "$startTime ~ $endTime"
+    }
+}
+
+// 2022-12-15T19:26
+private const val EventRawDateFormat = "yyyy-MM-dd'T'HH:mm"
+
+@VisibleForTesting
+internal fun String.toDate(): Date? {
+    val formatter = SimpleDateFormat(EventRawDateFormat, Locale.KOREA)
+    return formatter.parse(this)
+}
+
+@VisibleForTesting
+internal fun Date.isSameDay(other: Date): Boolean {
+    val cal1 = Calendar.getInstance().also { time = time }
+    val cal2 = Calendar.getInstance().also { time = other.time }
+    return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+            cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR)
 }
