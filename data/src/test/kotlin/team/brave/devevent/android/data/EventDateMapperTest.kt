e34493ed99cb345da213485a4c22fbc341b33269
jisungbin <ji@sungb.in>
2023-01-19 21:41:07 +0900

    test: 백엔드 날짜 포멧 변경 대응

--- data/src/test/kotlin/team/brave/devevent/android/data/EventDateMapperTest.kt
+++ data/src/test/kotlin/team/brave/devevent/android/data/EventDateMapperTest.kt
@@ -20,23 +20,23 @@ import team.brave.devevent.android.data.mapper.toEventDateString
 class EventDateMapperTest {
     @Test
     fun `이벤트 카드 형식에 맞는 날짜 변환 - 다른 일정`() {
-        val startDate = "2023-01-02T06:00"
-        val endDate = "2023-01-03T08:00"
-        val dateString = (EventDate.from(startDate) to EventDate.from(endDate)).toEventDateString()
+        val startDate = "2023-01-02 06:00"
+        val endDate = "2023-01-03 08:00"
+        val actual = (EventDate.from(startDate) to EventDate.from(endDate)).toEventDateString()
 
         val expected = "2023.01.02(월) ~ 2023.01.03(화)"
 
-        expectThat(dateString).isEqualTo(expected)
+        expectThat(actual).isEqualTo(expected)
     }
 
     @Test
     fun `이벤트 카드 형식에 맞는 날짜 변환 - 같은 일정`() {
-        val startDate = "2023-01-02T06:00"
-        val endDate = "2023-01-02T08:00"
-        val dateString = (EventDate.from(startDate) to EventDate.from(endDate)).toEventDateString()
+        val startDate = "2023-01-02 06:00"
+        val endDate = "2023-01-02 08:00"
+        val actual = (EventDate.from(startDate) to EventDate.from(endDate)).toEventDateString()
 
         val expected = "2023.01.02(월) 06:00 ~ 08:00"
 
-        expectThat(dateString).isEqualTo(expected)
+        expectThat(actual).isEqualTo(expected)
     }
 }
